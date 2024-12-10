200da72f84e2431d323318c74d0403523d439b39
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 20:03:50 +0530

    feat: add support for summing of two numbers seprated by ,

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,6 +2,11 @@ class StringCalculator{
 
   add(string){
     if (!string) return 0; //checking for empty string
+    const numArray=string.split(",").map(Number); //spilting string with , to numArray
+    if(numArray.length>1){
+      return numArray.reduce((sum, num) => sum + num, 0); //summing the array
+    }
+    
     return parseInt(string, 10);//converting single input to string
   }
   
