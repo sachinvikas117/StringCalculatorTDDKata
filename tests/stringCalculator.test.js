9d462660cce837832d2c5cea8f789aa1cef83e5a
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 19:53:25 +0530

    test: add failing test for single number input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -11,6 +11,10 @@ describe('StringCalculator', () => {
     expect(calculator.add("")).toBe(0);
   });
 
+  test('should return the number itself for a single number', () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
 });
 
 
