38ed936e596348556acaa861ba33320452841085
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 19:49:31 +0530

    test: add failing test for empty string returning 0

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,6 +7,10 @@ describe('StringCalculator', () => {
     calculator = new StringCalculator();
   });
 
+  test('should return 0 for an empty string', () => {
+    expect(calculator.add("")).toBe(0);
+  });
+
 });
 
 
