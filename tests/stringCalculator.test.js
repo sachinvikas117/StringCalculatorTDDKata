d4f3a1d2deb2023874e919409ea40a3d8c1f3cd2
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 19:46:38 +0530

    test: add failing test for class existence

---
+++ tests/stringCalculator.test.js
@@ -0,0 +1,13 @@
+const StringCalculator = require('../src/StringCalculator');
+
+describe('StringCalculator', () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+});
+
+
+
