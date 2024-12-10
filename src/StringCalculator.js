0e6ceecd1598f189bfeed2ffd9e620f6ed392440
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 20:15:54 +0530

    feat: add support for delimiters contain new lines and ,

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,7 +2,8 @@ class StringCalculator{
 
   add(string){
     if (!string) return 0; //checking for empty string
-    const numArray=string.split(",").map(Number); //spilting string with , to numArray
+    let delimiter = /,|\n/; //  delimiters contianing new line and ,
+    const numArray=string.split(delimiter).map(Number); //spilting string with , to numArray
     return numArray.reduce((sum, num) => sum + num, 0);
   }
   
