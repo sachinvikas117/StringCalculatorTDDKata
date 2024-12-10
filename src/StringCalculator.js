2946a159db5989be76a621bae039e8ae90002150
Sachin <sachin.vikas@rediffmail.com>
2024-12-10 20:59:01 +0530

    feat: add support for custom delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,6 +3,14 @@ class StringCalculator{
   add(string){
     if (!string) return 0; //checking for empty string
     let delimiter = /,|\n/; //  delimiters contianing new line and ,
+    const customDelimiterMatch = string.match(/^\/\/(\[.*?\]|\S+)\n/);
+    if (customDelimiterMatch) {
+      const customDelimiterSection = customDelimiterMatch[1];
+      string = string.slice(customDelimiterMatch[0].length);
+      delimiter = new RegExp(
+        [customDelimiterSection].map(d => d.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')).join('|'),
+        'g')
+    }
     const numArray=string.split(delimiter).map(Number); //spilting string with , to numArray
     return numArray.reduce((sum, num) => sum + num, 0);
   }
